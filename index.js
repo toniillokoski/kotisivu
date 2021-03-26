65489f568f7400bdde02656d671773adb75c3cdc
Toni Illokoski <toni.illokoski@gmail.com>
2021-03-26 16:48:34 +0200

    Form + links fixed

--- index.js
+++ index.js
@@ -20,3 +20,15 @@ $('a[href^="#"]').on('click', function(event) {
 
 initSr();
 initTilt();
+
+
+function submitForm() {
+  // Get the first form with the name
+  // Usually the form name is not repeated
+  // but duplicate names are possible in HTML
+  // Therefore to work around the issue, enforce the correct index
+  var frm = document.getElementById('contact_form')[0];
+  frm.submit(); // Submit the form
+  frm.reset();  // Reset all form data
+  return false; // Prevent page refresh
+}
